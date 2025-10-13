141ed08ad7cd9555a273c48eb62217d0d0099914
s0meq <rrramunaa@gmail.com>
2025-10-13 23:45:39 +0300

    5 - 9

--- assignment06/script.js
+++ assignment06/script.js
@@ -1,6 +1,6 @@
 // 1. Hello, World!
 function naytaHeiMaailma() {
-    document.getElementById("tulos1").innerHTML = "Hei, maailma!"
+    document.getElementById("tulos1").innerHTML = "Hei, maailma!";
 }
 
 // 2.
@@ -27,15 +27,66 @@ function laske3() {
 
 // 4.
 function tarkistaIka4() {
-    document.getElementById("tulos4").innerHTML = (Number(document.getElementById("ika4").value) < 18) ? "Olet alaikäinen" : "Olet täysi-ikäinen"
+    document.getElementById("tulos4").innerHTML = (Number(document.getElementById("ika4").value) < 18) ? "Olet alaikäinen" : "Olet täysi-ikäinen";
 }
 
 // 5.
 function tulostaNumerot5() {
-
+    let numbers = [];
+    for (let i = 0; i < 10; i++) {
+        numbers.push(i + 1);
+    }
+    document.getElementById("tulos5").innerHTML = numbers.join(" ");
 }
 
 // 6.
 function fizzBuzz6() {
+    let fizzBuzzArr = [];
+    for (let i = 1; i < 51; i++) {
+        if (i % 3 === 0 && i % 5 === 0){
+            fizzBuzzArr.push(i + " FizzBuzz!");
+        } else if (i % 3 === 0) {
+            fizzBuzzArr.push(i + " Fizz!");
+        } else if (i % 5 === 0) {
+            fizzBuzzArr.push(i + " Buzz!");
+        } else {
+            fizzBuzzArr.push(i);
+        }
+    }
+    document.getElementById("tulos6").innerHTML = fizzBuzzArr.join("<br>");
+}
+
+// 7.
+function laskeNelio7() {
+    let sqrRoot = document.getElementById("luku7").value;
+    document.getElementById("tulos7").innerHTML = sqrRoot * sqrRoot;
+}
+
+// 8.
+function luoAuto8() {
+    let brand = document.getElementById("merkki8").value;
+    let model = document.getElementById("malli8").value;
+    let year = Number(document.getElementById("vuosi8").value);
+    document.getElementById("tulos8").innerHTML = "Autosi on " + year + " " + brand + " " + model;
+}
+
+// 9.
+function laskeSumma9() {
     
+    let splitStr = document.getElementById("luvut9").value.split(",");
+    let sumOfString = 0;
+    for (let i = 0; i < splitStr.length; i++) {
+        sumOfString += Number(splitStr[i]);
+    }
+    document.getElementById("tulos9").innerHTML = sumOfString;
+}
+
+// 10.
+function laskeSanat10() {
+    // TODO count words from a text area input
+}
+
+// 11.
+function tarkistaPalindromi11() {
+    // TODO check if a word is a palindrome
 }
